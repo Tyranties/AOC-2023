f53105618adb31ef89d63dc6063fa6e6c4b1621b
TerenceCLZhang <terence.zhang2014@gmail.com>
2023-12-06 14:49:52 +1300

    Better Day 5

--- day_05/day_05_part_1.py
+++ day_05/day_05_part_1.py
@@ -1,45 +1,25 @@
-from itertools import groupby
-
-with open("day_05_test.txt", "r") as file:
+with open("day_05.txt", "r") as file:
     data = file.read()
-    data = data.split("\n")
+    seeds, *data_blocks = data.split("\n\n")
 
-almanac_sections = [list(group) for key, group in groupby(
-    data, key=lambda x: x == "") if not key]
+seeds = list(map(int, seeds.split(": ")[1].split()))
 
-seeds = []
-mappings_list = [{} for _ in range(7)]
+for data_block in data_blocks:
+    ranges_list = []
+    for line in data_block.splitlines()[1:]:
+        ranges_list.append(list(map(int, line.split())))
 
-for i, almanac_section in enumerate(almanac_sections):
-    if i == 0:
-        almanac_section = almanac_section[0].split()
-        for item in almanac_section[1:]:
-            seeds.append(int(item))
-        continue
+    new_seeds = []
 
-    almanac_section = almanac_section[1:]
-
-    for mapping in almanac_section:
-        numbers = mapping.split()
-        destination_start = int(numbers[0])
-        source_range = (int(numbers[1]), int(numbers[1]) + int(numbers[2]))
-
-        mappings_list[i - 1][source_range] = destination_start
-
-for i in range(7):
-    current_mappping = mappings_list[i]
-    for i, x in enumerate(seeds):
-        found = False
-        for source, destination in current_mappping.items():
-            if x in range(source[0], source[1] + 1):
-                difference = x - source[0]
-                new_val = destination + difference
-                found = True
+    for seed in seeds:
+        for destination, source, length in ranges_list:
+            if seed in range(source, source + length + 1):
+                new_seeds.append(seed - source + destination)
                 break
-        if not found:
-            new_val = x
+        else:
+            new_seeds.append(seed)
 
-        seeds[i] = new_val
+    seeds = new_seeds
 
 ans = min(seeds)
 print(ans)
